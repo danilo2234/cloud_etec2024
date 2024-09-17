d53045ef903170a626109c8d509abaeb64f3c4f2
unknown <danilo_23778@ETEC.FURLAN>
2024-09-17 11:04:18 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+//Instalar o express -
+//Gerenciador de pacotes - NPM
+const express = require('express')
+const server = express()
+//Importar para ter acesso ao caminho dos arquivos
+const path = require('path')
+//Expor arquivos estáticos..
+server.use(express.static('public'))
+server.use(express.json()) //Possibilidade de usar JSON
+//Eu desenvolvedor, criei este ENDPOINT
+server.get('/cadastro', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'))
+})
+
+server.get('/pets', (req,res)=>{
+    res.send({
+        name: "Meu gato",
+        idade: "4 anos",
+        peso: "300kg"
+    })
+})
+//Expor para o mundo na porta 3000
+server.listen(3000, () => {
+    console.log("servidor no ar...")
+})
